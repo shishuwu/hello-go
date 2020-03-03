b32f4714dc7de9735c65a744fd4d78ee767974c8
shishuwu <shuwu.shi@gmail.com>
2020-03-03 18:22:17 +0800

    readme & refer go inside module

---
+++ common/math/calc.go
@@ -0,0 +1,6 @@
+package math
+
+func Add(num1 int, num2 int) int {
+	sum := num1 + num2
+	return sum
+}
--- helloworld/hello.go
+++ helloworld/hello.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+//引用包内的 go 文件
+import "github.com/shishuwu/hello-go/common/math"
+
 func main() {
 	fmt.Println("Hello World!")
+
+	sum := math.Add(1,2)
+
+	fmt.Println(sum)
 }
